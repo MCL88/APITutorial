4c2a1da57d81191dad4038b8105df563ea16b601
MCL88 <miccel88@gmail.com>
2017-08-02 17:51:43 +0200

    Correzione stringhe(apice inverso)

--- main.go
+++ main.go
@@ -9,44 +9,49 @@ import
 )
 
 type Person struct {
-	ID 			string 		'json:"id,omitempty'
-	FirstName 	string 		'json:"firstname,omitempty"'
-	LastName 	string 		'json:"lastname,omitempty"'
-	Address 	*Address	'json:"address, omitempty'
+	ID 			string 		`json:"id,omitempty"` 
+	FirstName 	string 		`json:"firstname,omitempty"`
+	LastName 	string 		`json:"lastname,omitempty"`
+	Address 	*Address	`json:"address, omitempty`
 }
 
 type Address struct {
-	City 	string 'json:"city,omitempty'		
-	State 	string 'json:"state,omitempty'		
+	City 	string `json:"city,omitempty"`		
+	State 	string `json:"state,omitempty"`		
 }
 
 var people []Person
 
-func GetPersonEndpoint(w http.ResposeWriter, req *http.Request) {
-
+func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for _, item := range(people) {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+		}
+	}
 }
 
-func GetPeopleEndpoint(w http.ResposeWriter, req *http.Request) {
+func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 
-func UpdatePersonEndpoint(w http.ResposeWriter, req *http.Request) {
+func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func DeletePersonEndpoint(w http.ResposeWriter, req *http.Request) {
+func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 
 }
 
 func main() {
 //Istanza del router dell'applicazione
 	router := mux.NewRouter()
-	people := append(people, Person{ID: "1", Firstname: "Michele", Lastname:"Maresca", Address:&Address{City:"Messina", State:"Italy"}})
-	people := append(people, Person{ID: "2", Firstname: "Alessio", Lastname:"Maresca", Address:&Address{City:"Messina", State:"Italy"}})
+	people = append(people, Person{ID: "1", FirstName: "Michele", LastName:"Maresca", Address:&Address{City:"Messina", State:"Italy"}})
+	people = append(people, Person{ID: "2", FirstName: "Alessio", LastName:"Maresca", Address:&Address{City:"Messina", State:"Italy"}})
 	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPeopleEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePeopleEndpoint).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePeopleEndpoint).Methods("DELETE")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
 //http oggetto che gestice il server
 	log.Fatal(http.ListenAndServe(":12345", router))
 }
